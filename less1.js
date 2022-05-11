2186962a4db580ddac38fcb5a9ef74da641bd360
baidayuliia <92315403+baidayuliia@users.noreply.github.com>
2022-05-11 18:00:49 +0300

    Create less1.js

---
+++ less1.js
@@ -0,0 +1,9 @@
+let userName = "Juliia"
+let userAge = 16
+let userHobby = "Sing"
+
+console.log("User name "+ userName, "User age " + userAge, "User hobby? " + userHobby)
+
+alert("Hello " + userName)
+alert("You " + userAge)
+alert("You hobby " + userHobby)
